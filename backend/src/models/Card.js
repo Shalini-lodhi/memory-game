5bcafb0074c80d6abaa8ffe6c7c739350a046ebf
Shalini-lodhi <shalinilodhi01@gmail.com>
2025-04-01 12:20:46 +0530

    backend-test-lib

--- backend/src/models/Card.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CardSchema = new mongoose.Schema({
-  theme_id: { type: mongoose.Schema.Types.ObjectId, ref: "Theme", required: true },
-  word: { type: String, required: true },
-  is_active: { type: Boolean, default: true },
-});
-
-module.exports = mongoose.model("Card", CardSchema);
